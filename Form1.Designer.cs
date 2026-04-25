f1ade7069c92730a81b782b56378b67b2d97fdfd
Lu-Gpt <lukaaddress123@gmail.com>
2026-04-25 16:35:12 +0800

    feat(core): 替代還原FileHistory的功能

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FileHistoryCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SelectDirectory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(144, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(627, 33);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.GreenYellow;
+            this.button2.Location = new System.Drawing.Point(320, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Transport";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 177);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(338, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Copy To";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(338, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FileHistory";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(144, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(627, 33);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SelectPastePath";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 209);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "FileHistoryCopy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using FileHistoryCopy.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileHistoryCopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "E:\\FileHistory\\user";
+            textBox2.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = path;
+            }
+            else
+            {
+                MessageBox.Show("請指定Folder路徑!");
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowserDialog1.SelectedPath;
+                textBox2.Text = path;
+            }
+            else
+            {
+                MessageBox.Show("請指定Folder路徑!");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty && Directory.Exists(textBox1.Text) &&
+                textBox2.Text != string.Empty && Directory.Exists(textBox2.Text))
+            {
+                IEnumerable<string> d1 = Directory.EnumerateFiles(textBox1.Text, "*", SearchOption.AllDirectories);
+
+                var latestFiles = GetFileList(d1).GroupBy(f => f)
+                                    .Select(g => g.OrderByDescending(f => f.TimeStamp).First())
+                                    .ToList();
+
+                #region
+                /*
+                // 1. 這是你當初掃描的起始點 (備份來源的根目錄)
+                string backupRoot = textBox1.Text;
+                string destinationRoot = textBox2.Text;
+
+                if(!destinationRoot.EndsWith(Path.GetFileName(backupRoot)))
+                {
+                    destinationRoot = Path.Combine(destinationRoot,Path.GetFileName(backupRoot));
+                }
+
+                foreach (FileDetail fd in latestFiles)
+                {
+                    // 2. 計算相對路徑 (重要！)
+                    // 例如: fd.DirectoryPath 是 "D:\BackupSource\FolderA\SubFolderB"
+                    // 得到的 relativeDir 就是 "\FolderA\SubFolderB"
+                    string relativeDir = fd.DirectoryPath.Replace(backupRoot, "");
+
+                    // 3. 組合出目標資料夾的完整路徑
+                    // 結果: "E:\Restore\FolderA\SubFolderB"
+                    string targetFolder = Path.Combine(destinationRoot, relativeDir.TrimStart(Path.DirectorySeparatorChar));
+
+                    // 4. 確保多層資料夾一次建立
+                    if (!Directory.Exists(targetFolder))
+                    {
+                        // CreateDirectory 很強大，它會自動建立中間缺少的每一層資料夾
+                        Directory.CreateDirectory(targetFolder);
+                    }
+
+                    // 5. 組合最終檔案路徑並複製
+                    string destinationPath = Path.Combine(targetFolder, fd.FileName);
+                    File.Copy(fd.FullFileName, destinationPath, true);
+                }
+                */
+                #endregion
+                #region
+                // 1. 這是你當初掃描的起始點 (備份來源的根目錄)
+                string backupRoot = textBox1.Text.TrimEnd(Path.DirectorySeparatorChar);
+                string destinationRoot = textBox2.Text;
+
+                // 1. 確保目標資料夾內包含來源資料夾的名稱
+                string backupDirName = Path.GetFileName(backupRoot);
+                if (!destinationRoot.TrimEnd(Path.DirectorySeparatorChar).EndsWith(backupDirName, StringComparison.OrdinalIgnoreCase))
+                {
+                    destinationRoot = Path.Combine(destinationRoot, backupDirName);
+                }
+
+                foreach (FileDetail fd in latestFiles)
+                {
+                    // 2. 用長度擷取相對路徑，比 Replace 更安全
+                    string relativeDir = fd.DirectoryPath.Length > backupRoot.Length
+                                         ? fd.DirectoryPath.Substring(backupRoot.Length).TrimStart(Path.DirectorySeparatorChar)
+                                         : "";
+
+                    // 3. 組合目標資料夾
+                    string targetFolder = Path.Combine(destinationRoot, relativeDir);
+
+                    // 4. 建立資料夾
+                    if (!string.IsNullOrEmpty(targetFolder) && !Directory.Exists(targetFolder))
+                    {
+                        Directory.CreateDirectory(targetFolder);
+                    }
+
+                    // 5. 複製檔案
+                    string destinationPath = Path.Combine(targetFolder, fd.FileName);
+                    File.Copy(fd.FullFileName, destinationPath, true);
+                }
+                #endregion
+            }
+            else
+            {
+                MessageBox.Show("找不到路徑");
+            }
+        }
+        private IEnumerable<FileDetail> GetFileList(IEnumerable<string> pathlist)
+        {
+            if (pathlist != null && pathlist.Any())
+            {
+                List<FileDetail> list = new List<FileDetail>();
+                foreach (string path in pathlist)
+                {
+                    if (File.Exists(path))
+                    {
+                        using (FileStream file1 = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                        {
+                            FileDetail fd = new FileDetail();
+                            fd.FullFileName = file1.Name;
+                            fd.FileLength = file1.Length;
+                            fd.LastWriteTime = File.GetLastWriteTimeUtc(path);
+
+                            string pattern = @"\s\((?<timestamp>\d{4}_\d{2}_\d{2}\s\d{2}_\d{2}_\d{2})\sUTC\)$";
+
+                            Match match = Regex.Match(Path.GetFileNameWithoutExtension(path), pattern);
+                            if (match.Success)
+                            {
+                                string rawTimestamp = match.Groups["timestamp"].Value;
+                                //須注意Kind要是UTC無偏移
+                                fd.TimeStamp = DateTime.ParseExact(rawTimestamp, "yyyy_MM_dd HH_mm_ss", 
+                                    CultureInfo.InvariantCulture,
+                                    DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal);
+                            }
+
+                            string nameWithoutExt = Path.GetFileNameWithoutExtension(file1.Name);
+
+                            string cleanMainName = Regex.Replace(nameWithoutExt, pattern, "");
+
+                            fd.FileName = cleanMainName + Path.GetExtension(file1.Name);
+                            list.Add(fd);
+                        }
+                    }
+                }
+                return list;
+            }
+            return Enumerable.Empty<FileDetail>();
+        }
+    }
+}
